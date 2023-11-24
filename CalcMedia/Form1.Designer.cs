14b7ad1fa380e6a89bab7f1731abf806989776a5
apdpa <apdpa@XOCO>
2023-11-24 03:47:06 -0300

    Add project files.

---
+++ CalcMedia/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace CalcMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxNota1 = new System.Windows.Forms.TextBox();
+            this.textBoxNota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNota3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(144, 322);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nota 1 = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(315, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "CALCULADORA DE MÉDIA HARMÔNICA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 198);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Sua média é:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(171, 228);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "media";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(103, 425);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(202, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Criado por: Eng. Álisson Pinheiro";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(144, 275);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxNota1
+            // 
+            this.textBoxNota1.Location = new System.Drawing.Point(130, 80);
+            this.textBoxNota1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxNota1.Name = "textBoxNota1";
+            this.textBoxNota1.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNota1.TabIndex = 1;
+            // 
+            // textBoxNota2
+            // 
+            this.textBoxNota2.Location = new System.Drawing.Point(130, 116);
+            this.textBoxNota2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxNota2.Name = "textBoxNota2";
+            this.textBoxNota2.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNota2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nota 2 = ";
+            // 
+            // textBoxNota3
+            // 
+            this.textBoxNota3.Location = new System.Drawing.Point(130, 154);
+            this.textBoxNota3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxNota3.Name = "textBoxNota3";
+            this.textBoxNota3.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNota3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nota 3 = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 450);
+            this.Controls.Add(this.textBoxNota3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNota2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNota1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA DE MÉDIA HARMÔNICA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxNota1;
+        private System.Windows.Forms.TextBox textBoxNota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNota3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalcMedia/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // calcular
+        {
+            // Obter as notas dos TextBox
+            if (double.TryParse(textBoxNota1.Text, out double nota1) &&
+                double.TryParse(textBoxNota2.Text, out double nota2) &&
+                double.TryParse(textBoxNota3.Text, out double nota3))
+            {
+                // Calcular a média harmônica
+                double mediaHarmonica = CalcularMediaHarmonica(nota1, nota2, nota3);
+
+                // Exibir o resultado em um MessageBox
+                label6.Text = $"{mediaHarmonica:F2}";
+            }
+            
+            else
+            {
+                MessageBox.Show("Por favor, insira valores numéricos válidos para as notas.", "Erro de entrada");
+            }
+        }
+        // Função para calcular a média harmônica
+        private double CalcularMediaHarmonica(double a, double b, double c)
+        {
+            return 3.0 / ((1.0 / a) + (1.0 / b) + (1.0 / c));
+        }
+
+        private void button2_Click(object sender, EventArgs e) // limpar
+        {
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    ((TextBox)controle).Text = "";
+                }
+            }
+        }
+    }
+}
